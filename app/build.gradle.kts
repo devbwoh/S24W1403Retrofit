473391765d914cb2782e841e4618b8005e735961
BWOH <bwoh@outlook.kr>
2024-12-09 23:59:39 +0900

    🍱 AsyncImage 사용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,12 @@ android {
 
 dependencies {
 
+    // NOTE: AsyncImage 사용하기 위해 추가
+    //implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation(libs.coil.compose)
+    //implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
+    implementation(libs.coil.network.okhttp)
+
     // NOTE: Retrofit 사용하기 위해 추가
     //implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation(libs.retrofit)
--- app/src/main/java/kr/ac/kumoh/ce/s20240000/s24w1403retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20240000/s24w1403retrofit/MainActivity.kt
@@ -7,10 +7,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,9 +29,14 @@ import kr.ac.kumoh.ce.s20240000.s24w1403retrofit.ui.theme.S24W1403RetrofitTheme
 import androidx.compose.runtime.getValue
 
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,20 +77,40 @@ fun SongList(list: List<Song>, modifier: Modifier) {
 
 @Composable
 fun SongItem(song: Song) {
-    Column(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color(0xffffffcc))
-            .padding(16.dp)
+            .height(IntrinsicSize.Min)
+            .background(Color(255, 210, 210))
+            .padding(8.dp)
     ) {
-        TextTitle(song.title)
-        TextSinger(song.singer)
+        AsyncImage(
+            model = "https://picsum.photos/300/300?random=${song.singer}",
+            contentDescription = "가수 이미지 ${song.singer}",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                //.clip(CircleShape),
+                .clip(RoundedCornerShape(percent = 10)),
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            TextTitle(song.title)
+            TextSinger(song.singer)
+        }
     }
 }
 
 @Composable
 fun TextTitle(title: String) {
-    Text(title, fontSize = 30.sp)
+    Text(
+        title,
+        fontSize = 30.sp,
+        lineHeight = 35.sp
+    )
 }
 
 @Composable
